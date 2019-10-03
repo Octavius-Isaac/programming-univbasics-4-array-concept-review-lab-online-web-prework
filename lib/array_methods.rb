c46bd4bd20043978883e11b427e098af3e6a216e
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-10-03 21:26:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,8 +11,10 @@ end
 
 def find_max_value(array)
   x = array[0]
-  array.length.times > {|index|
-  if array.index 
+  array.length.times {|index|
+  if array.index > x 
+    
+  end
 end
 
 def find_min_value(array)
